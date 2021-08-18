3526abbe376e84c24db96c5b535b7df3bcad894a
Crowdbotics <team@crowdbotics.com>
2021-08-18 17:10:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "raspy_salad_29916Identifier",
+  url: "https://raspy_salad_29916Identifier.botics.co",
+  api: "https://raspy_salad_29916Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
